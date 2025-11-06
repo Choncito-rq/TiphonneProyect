5c5c841c93426fc0dfbf16b7144651f291b1d5f1
luisDVSS <8elilustrador8@gmail.com>
2025-11-05 22:38:24 -0700

    ver 0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+function Login() {
+  const [pswd, setPswd] = useState("");
+  const [email, setEmail] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //logica de validacion de correo
+    if ((pswd == "1234") & (email === "luis@gmail.com")) {
+      alert("listo");
+      token = generateToken();
+    } else {
+      alert("datos incorrectos");
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="login-container">
+      <h1>Login :D</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          className="login-input"
+          type="email"
+          value={email}
+          placeholder="Correo electrónico"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          className="login-input"
+          type="password"
+          value={pswd}
+          placeholder="Contraseña"
+          onChange={(e) => setPswd(e.target.value)}
+          required
+        />
+        <button className="login-button" type="submit">
+          Iniciar sesión
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <a className="login-a" href="#">
+          Olvidé mi contraseña
+        </a>
+      </form>
+    </div>
   );
 }
-
-export default App;
+export default function App() {
+  return <Login />;
+}
