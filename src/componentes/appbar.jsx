573ce061b0bd40689e121c5357673045f84053a3
luisDVSS <8elilustrador8@gmail.com>
2025-11-06 22:31:29 -0700

    a

--- src/componentes/appbar.jsx
+++ src/componentes/appbar.jsx
@@ -2,9 +2,8 @@ import "./appbar.css";
 export default function Appbar() {
   return (
     <header className="appbar">
-      <h1 className="logo">Mi App</h1>
+      <h1 className="logo">Tiphonne</h1>
       <nav>
-        <a href="/">Inicio</a>
         <a href="/perfil">Perfil</a>
         <a href="/config">Configuración</a>
       </nav>
